9a5a239a71b07be5b73bdfae0f0e81c319d61196
maryamzeynal0va <maryamzeynalovaa@gmail.com>
2026-07-14 23:17:09 +0400

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = 3000;
+
+const tasks = [
+    {
+        id: 0,
+        title: "Learn JavaScript",
+        completed: false
+    },
+    {
+        id: 1,
+        title: "Finish Assignment",
+        completed: true
+    },
+    {
+        id: 2,
+        title: "Check Emails",
+        completed: false
+    }
+];
 
 // Root endpoint
 app.get("/", (req, res) => {
@@ -17,6 +33,24 @@ app.get("/health", (req, res) => {
     res.json({
         status: "ok"
     });
+});
+
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        });
+    }
+
+    res.json(task);
 });
 
 app.listen(PORT, () => {
