7f8d06e21e3982fbbb4cba54a0b7b5161c0d8f39
maryamzeynal0va <maryamzeynalovaa@gmail.com>
2026-07-15 04:34:05 +0400

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -71,6 +71,53 @@ app.post("/tasks", (req, res) => {
     tasks.push(newTask);
     res.status(201).json(newTask);
 });
+
+app.put("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    const { title, done } = req.body;
+
+    if (title === undefined && done === undefined) {
+        return res.status(400).json({
+            error: "Request body is empty"
+        });
+    }
+
+    if (title !== undefined) {
+        task.title = title;
+    }
+
+    if (done !== undefined) {
+        task.done = done;
+    }
+
+    res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const index = tasks.findIndex(task => task.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(index, 1);
+
+    res.sendStatus(204);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
